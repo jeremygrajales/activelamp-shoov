671863d4243bfaf0ca190822f525a63b8b818366
Jonathan Westman <jwestman@activelamp.com>
2015-11-10 11:25:56 -0800

    initial commit

---
+++ visual-monitor/test/tests.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var shoovWebdrivercss = require('shoov-webdrivercss');
+var config = require('../configuration.js');
+
+describe('Visual monitor testing', function() {
+
+  this.timeout(99999999);
+  var client = {};
+
+  before(function(done){
+    client = shoovWebdrivercss.before(done, config.caps);
+  });
+
+  after(function(done) {
+    shoovWebdrivercss.after(done);
+  });
+
+  it('should show the home page',function(done) {
+    client
+      .url(config.baseUrl)
+      .webdrivercss(config.testName + '.homepage', {
+        name: '1',
+        exclude: [],
+        remove: [],
+        hide: [],
+        screenWidth: config.selectedCaps == 'chrome' ? [640, 960, 1200] : undefined,
+      }, config.resultsCallback)
+      .call(done);
+  });
+});
